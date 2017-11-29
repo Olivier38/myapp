11d63f28b4d0234edad08a0a6ba1a3a89bff2157
Olivier Traveaux <olivier.traveaux@gmail.com>
2017-11-29 11:11:11 +0100

    blank app with click & back btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { FirstPage } from '../pages/first/first';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FirstPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    FirstPage
   ],
   providers: [
     StatusBar,
